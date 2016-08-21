495b16b4ecbe6e5c0876cb8d17b14d02e2b47ddc
fanxing <lwl139678@139.com>
2016-08-21 16:41:11 +0800

    feature 添加解析Json协议的方法

---
+++ src/com/llxx/socket/protocol/ProtocolJson.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.llxx.socket.protocol;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.llxx.socket.loger.Ll_Loger;
+import com.llxx.socket.wrap.bean.Ll_Message;
+
+/**
+ * @author 繁星
+ * @describe Json协议封装
+ */
+public class ProtocolJson implements IProtocol
+{
+    public static final String TAG = "ProtocolJson";
+
+    @Override
+    public Protocol parseMessage(Ll_Message message)
+    {
+        try
+        {
+            JSONObject object = new JSONObject(message.getMessage());
+            String action = object.optString("action", "");
+            Class<? extends Protocol> protocol = ProtocolManager.mProtocols
+                    .get(action);
+            Ll_Loger.d(TAG, "parseMessage -> " + message.getMessage()
+                    + ", protocol ->" + protocol);
+            if (protocol != null)
+            {
+                try
+                {
+                    Protocol mProtocol = protocol.newInstance();
+                    mProtocol.setMessage(message);
+                    mProtocol.prase();
+                    return mProtocol;
+                }
+                catch (InstantiationException e)
+                {
+                    e.printStackTrace();
+                }
+                catch (IllegalAccessException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/com/llxx/socket/wrap/bean/Ll_Message.java
+++ src/com/llxx/socket/wrap/bean/Ll_Message.java
@@ -1,13 +1,33 @@
 package com.llxx.socket.wrap.bean;
 
+/**
+ * 
+ * @author 繁星
+ * @describe 终端发送过来的消息，分装了消息的内容，以及消息的封装格式
+ */
 public class Ll_Message
 {
-    private String message;
+    /**
+     * 消息使用Json作为数据格式传输
+     */
+    public static final int MSG_JSON = 0x01;
+
+    /**
+     * 消息使用 action|params... 的格式传输
+     */
+    public static final int MSG_SPLIT = 0x02;
+
+    private String message = "";
+    private int msgtype = MSG_SPLIT;
 
     public Ll_Message(String message)
     {
         super();
         this.message = message;
+        if (message.startsWith("{") && message.endsWith("}"))
+        {
+            msgtype = MSG_JSON;
+        }
     }
 
     /**
@@ -26,4 +46,19 @@ public class Ll_Message
         this.message = message;
     }
 
+    /**
+     * @return the msgtype
+     */
+    public int getMsgtype()
+    {
+        return msgtype;
+    }
+
+    /**
+     * @param msgtype the msgtype to set
+     */
+    public void setMsgtype(int msgtype)
+    {
+        this.msgtype = msgtype;
+    }
 }
