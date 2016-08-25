abdc4fa8ead0efd0eecb57c41ee81106b6b1daf1
fanxing <lwl139678@139.com>
2016-08-25 13:01:20 +0800

    feature 添加打开辅助功能的按钮

--- src/com/llxx/socket/MainActivity.java
+++ src/com/llxx/socket/MainActivity.java
@@ -6,6 +6,7 @@ import com.llxx.utils.BinderUtils;
 import com.llxx.utils.ToastUtil;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -28,6 +29,7 @@ public class MainActivity extends Activity implements OnClickListener
         findViewById(R.id.socket_send).setOnClickListener(this);
         findViewById(R.id.open_toast).setOnClickListener(this);
         findViewById(R.id.start_second_page).setOnClickListener(this);
+        findViewById(R.id.start_fuzhu_fuwu).setOnClickListener(this);
     }
 
     int start = 0;
@@ -42,6 +44,20 @@ public class MainActivity extends Activity implements OnClickListener
             {
                 mBinderUtils.getService().sendMessage("socket_send");
                 Ll_Loger.d(TAG, "socket_send->socket_send");
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            break;
+
+        case R.id.start_fuzhu_fuwu:
+            try
+            {
+                Intent intent = new Intent(
+                        "android.settings.ACCESSIBILITY_SETTINGS");
+                startActivity(intent);
+                
             }
             catch (Exception e)
             {
@@ -69,4 +85,46 @@ public class MainActivity extends Activity implements OnClickListener
             break;
         }
     }
+
+    /*
+     *  
+     com.android.settings.AccessibilitySettings 辅助功能设置
+     　　com.android.settings.ActivityPicker 选择活动
+     　　com.android.settings.ApnSettings APN设置
+     　　com.android.settings.ApplicationSettings 应用程序设置
+     　　com.android.settings.BandMode 设置GSM/UMTS波段
+     　　com.android.settings.BatteryInfo 电池信息
+     　　com.android.settings.DateTimeSettings 日期和坝上旅游网时间设置
+     　　com.android.settings.DateTimeSettingsSetupWizard 日期和时间设置
+     　　com.android.settings.DevelopmentSettings 应用程序设置=》开发设置
+     　　com.android.settings.DeviceAdminSettings 设备管理器
+     　　com.android.settings.DeviceInfoSettings 关于手机
+     　　com.android.settings.Display 显示——设置显示字体大小及预览
+     　　com.android.settings.DisplaySettings 显示设置
+     　　com.android.settings.DockSettings 底座设置
+     　　com.android.settings.IccLockSettings SIM卡锁定设置
+     　　com.android.settings.InstalledAppDetails 语言和键盘设置
+     　　com.android.settings.LanguageSettings 语言和键盘设置
+     　　com.android.settings.LocalePicker 选择手机语言
+     　　com.android.settings.LocalePickerInSetupWizard 选择手机语言
+     　　com.android.settings.ManageApplications 已下载（安装）软件列表
+     　　com.android.settings.MasterClear 恢复出厂设置
+     　　com.android.settings.MediaFormat 格式化手机闪存
+     　　com.android.settings.PhysicalKeyboardSettings 设置键盘
+     　　com.android.settings.PrivacySettings 隐私设置
+     　　com.android.settings.ProxySelector 代理设置
+     　　com.android.settings.RadioInfo 手机信息
+     　　com.android.settings.RunningServices 正在运行的程序（服务）
+     　　com.android.settings.SecuritySettings 位置和安全设置
+     　　com.android.settings.Settings 系统设置
+     　　com.android.settings.SettingsSafetyLegalActivity 安全信息
+     　　com.android.settings.SoundSettings 声音设置
+     　　com.android.settings.TestingSettings 测试——显示手机信息、电池信息、使用情况统计、Wifi information、服务信息
+     　　com.android.settings.TetherSettings 绑定与便携式热点
+     　　com.android.settings.TextToSpeechSettings 文字转语音设置
+     　　com.android.settings.UsageStats 使用情况统计
+     　　com.android.settings.UserDictionarySettings 用户词典
+     　　com.android.settings.VoiceInputOutputSettings 语音输入与输出设置
+     　　com.android.settings.WirelessSettings 无线和网络设置
+     */
 }
