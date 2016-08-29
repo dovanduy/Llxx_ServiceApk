89d78f11da4501017cc5957a7bf3980a4e87a333
xingxing <lwl139678@139.com>
2016-08-29 22:32:30 +0800

    feautre Nodeinfo编码为json格式的统一

--- src/com/llxx/nodefinder/AccessibilityNodeInfoToJson.java
+++ src/com/llxx/nodefinder/AccessibilityNodeInfoToJson.java
@@ -24,52 +24,44 @@ public class AccessibilityNodeInfoToJson
     public static JSONObject getJson(AccessibilityNodeInfo info) throws JSONException
     {
         JSONObject object = new JSONObject();
+
+        object.put("text", CharSequenceWrap(info.getText()));
+
+        object.put("windowId", info.getWindowId());
+        object.put("index", -1);
+        try
+        {
+            object.put("resource-id", callIntMethod("getAccessibilityViewId", info));
+        }
+        catch (Exception e)
+        {
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2)
+        {
+            object.put("resource-id", CharSequenceWrap(info.getViewIdResourceName()));
+        }
+
+
         object.put("package", CharSequenceWrap(info.getPackageName()));
         object.put("class", CharSequenceWrap(info.getClassName()));
-        object.put("windowId", info.getWindowId());
+
         object.put("content-desc", info.getContentDescription());
+
         Rect rect = new Rect();
         info.getBoundsInScreen(rect);
-        object.put("bounds", "[" + rect.left + "," + rect.top+ "]" + "[" + rect.right + "," + rect.bottom+ "]");
-        
-        object.put("text", CharSequenceWrap(info.getText()));
+        object.put("bounds", "[" + rect.left + "," + rect.top + "]" + "[" + rect.right + "," + rect.bottom + "]");
+
         object.put("checkable", info.isCheckable());
         object.put("checked", info.isChecked());
         object.put("focusable", info.isFocusable());
         object.put("focused", info.isFocused());
         object.put("selected", info.isSelected());
         object.put("clickable", info.isClickable());
-        object.put("longClickable", info.isLongClickable());
+        object.put("long_clickable", info.isLongClickable());
         object.put("enabled", info.isEnabled());
         object.put("password", info.isPassword());
         object.put("scrollable", info.isScrollable());
-        //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
-        //        {
-        //            try
-        //            {
-        //                object.put("collectionInfoCol", info.getCollectionInfo().getColumnCount());
-        //                object.put("collectionInfoRow", info.getCollectionInfo().getRowCount());
-        //            }
-        //            catch (Exception e)
-        //            {
-        //                e.printStackTrace();
-        //            }
-        //        }
-
-        // object.put("nodedump", info.toString());
-        try
-        {
-            object.put("id", callIntMethod("getAccessibilityViewId", info));
-        }
-        catch (Exception e)
-        {
-        }
         
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2)
-        {
-            object.put("idname", CharSequenceWrap(info.getViewIdResourceName()));
-        }
-
         JSONArray array = new JSONArray();
         for (int i = 0; i < info.getChildCount(); i++)
         {
