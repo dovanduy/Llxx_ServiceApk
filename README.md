8b8f817e387bce4e91b1edbef700c8a6a637aeb1
fanxing <lwl139678@139.com>
2016-09-05 22:25:24 +0800

    feature 添加common库

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Llxx_MonkeyAutoTest的Android端服务程序，为其提供各种服务
 
 #注意事项：
 1、安装APK之后需要到设置->辅助服务中打开AutoTestService,否则部分功能无法正常使用
+
+
+#参考
+使用 android 公用库： https://github.com/Trinea/android-common
--- src/com/llxx/socket/service/Ll_AccessibilityService.java
+++ src/com/llxx/socket/service/Ll_AccessibilityService.java
@@ -64,79 +64,86 @@ public class Ll_AccessibilityService extends AccessibilityService
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event)
     {
-        mController.onAccessibilityEvent(event);
-        
-        // 处理自己的消息
-        AccessibilityNodeInfo nodeInfo = event.getSource();
-        String result = AccessibilityActionManager
-                .processEvent(getApplicationContext(), event, nodeInfo);
-        if (!TextUtils.isEmpty(result))
+        try
         {
-            sendMessage(result);
-            return;
-        }
-        // TODO Auto-generated method stub  
-        int eventType = event.getEventType();
-        String eventText = "";
-        if (DEBUG_OUTPUT)
-        {
-            Ll_Loger.i(TAG, "==============Start====================");
-        }
-        switch (eventType)
-        {
-        case AccessibilityEvent.TYPE_VIEW_CLICKED:
-            eventText = "TYPE_VIEW_CLICKED";
-            break;
-        case AccessibilityEvent.TYPE_VIEW_FOCUSED:
-            eventText = "TYPE_VIEW_FOCUSED";
-            break;
-        case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
-            eventText = "TYPE_VIEW_LONG_CLICKED";
-            break;
-        case AccessibilityEvent.TYPE_VIEW_SELECTED:
-            eventText = "TYPE_VIEW_SELECTED";
-            break;
-        case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
-            eventText = "TYPE_VIEW_TEXT_CHANGED";
-            break;
-        case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
-            eventText = "TYPE_WINDOW_STATE_CHANGED";
-            break;
-        case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
-            eventText = "TYPE_NOTIFICATION_STATE_CHANGED";
+            mController.onAccessibilityEvent(event);
+            
+            // 处理自己的消息
+            AccessibilityNodeInfo nodeInfo = event.getSource();
+            String result = AccessibilityActionManager
+                    .processEvent(getApplicationContext(), event, nodeInfo);
+            if (!TextUtils.isEmpty(result))
+            {
+                sendMessage(result);
+                return;
+            }
+            // TODO Auto-generated method stub  
+            int eventType = event.getEventType();
+            String eventText = "";
+            if (DEBUG_OUTPUT)
+            {
+                Ll_Loger.i(TAG, "==============Start====================");
+            }
+            switch (eventType)
+            {
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+                eventText = "TYPE_VIEW_CLICKED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_FOCUSED:
+                eventText = "TYPE_VIEW_FOCUSED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
+                eventText = "TYPE_VIEW_LONG_CLICKED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_SELECTED:
+                eventText = "TYPE_VIEW_SELECTED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
+                eventText = "TYPE_VIEW_TEXT_CHANGED";
+                break;
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                eventText = "TYPE_WINDOW_STATE_CHANGED";
+                break;
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
+                eventText = "TYPE_NOTIFICATION_STATE_CHANGED";
 
-            break;
-        case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END:
-            eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_END";
-            break;
-        case AccessibilityEvent.TYPE_ANNOUNCEMENT:
-            eventText = "TYPE_ANNOUNCEMENT";
-            break;
-        case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:
-            eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_START";
-            break;
-        case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:
-            eventText = "TYPE_VIEW_HOVER_ENTER";
-            break;
-        case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT:
-            eventText = "TYPE_VIEW_HOVER_EXIT";
-            break;
-        case AccessibilityEvent.TYPE_VIEW_SCROLLED:
-            eventText = "TYPE_VIEW_SCROLLED";
-            break;
-        case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED:
-            eventText = "TYPE_VIEW_TEXT_SELECTION_CHANGED";
-            break;
-        case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
-            eventText = "TYPE_WINDOW_CONTENT_CHANGED";
-            break;
-        }
-        eventText = eventText + ":" + eventType;
-        if (DEBUG_OUTPUT)
+                break;
+            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_END:
+                eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_END";
+                break;
+            case AccessibilityEvent.TYPE_ANNOUNCEMENT:
+                eventText = "TYPE_ANNOUNCEMENT";
+                break;
+            case AccessibilityEvent.TYPE_TOUCH_EXPLORATION_GESTURE_START:
+                eventText = "TYPE_TOUCH_EXPLORATION_GESTURE_START";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_HOVER_ENTER:
+                eventText = "TYPE_VIEW_HOVER_ENTER";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_HOVER_EXIT:
+                eventText = "TYPE_VIEW_HOVER_EXIT";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_SCROLLED:
+                eventText = "TYPE_VIEW_SCROLLED";
+                break;
+            case AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED:
+                eventText = "TYPE_VIEW_TEXT_SELECTION_CHANGED";
+                break;
+            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
+                eventText = "TYPE_WINDOW_CONTENT_CHANGED";
+                break;
+            }
+            eventText = eventText + ":" + eventType;
+            if (DEBUG_OUTPUT)
 
+            {
+                Ll_Loger.i(TAG, eventText);
+                Ll_Loger.i(TAG, "=============END=====================");
+            }
+        }
+        catch (Throwable e)
         {
-            Ll_Loger.i(TAG, eventText);
-            Ll_Loger.i(TAG, "=============END=====================");
+            e.printStackTrace();
         }
     }
 
